62e96752d21f736e46277842a40d5b1b31d2b4fa
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-12 16:45:30 -0300

    Arrow Functions

---
+++ arrow.js
@@ -0,0 +1,16 @@
+function soma(x, y) {
+    return x + y;
+}
+
+const multiplicacao = function(x, y) {
+    return x * y;
+};
+
+//console.log(multiplicacao(5,5));
+//Arrow
+const dividir = (x, y) => {
+    return  x / y;
+};
+
+const objeto = () => ({ nome: 'Natt', sobreNome: 'Souza'});
+
