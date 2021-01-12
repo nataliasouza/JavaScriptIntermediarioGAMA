3c50eed1b90b34d93ea22685868783ee844dc8ce
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-12 18:14:16 -0300

    Create spread.js

---
+++ spread.js
@@ -0,0 +1,15 @@
+let lista = [1, 2, 5];
+let listaIncluir = [3, 4];
+
+//console.log(lista);
+//console.log(listaIncluir);
+
+for (let i = 0; i < listaIncluir.length; i++) {
+    for (let j = 0; j < lista.length; j++) {
+        if (lista[j] >= listaIncluir[i]) {
+            lista.splice(j, 0, listaIncluir[i]);
+            j++;
+        }
+    }
+}
+console.log(lista);
